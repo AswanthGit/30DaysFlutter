f68411e76cd17b2238401e0dce688ebd36df6fdf
ASWANTH THANI <me19mtech10025@iith.ac.in>
2021-05-28 15:22:13 +0530

    day4

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final String loginImage = "assets/images/selfie.svg";
     return Material(
-      child: Center(
-          child: Text(
-        'Login page',
-        style: TextStyle(
-            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
-      )),
-    );
+        child: Column(
+      children: [
+        SvgPicture.asset(
+          loginImage,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Welcome',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Enter your Name', labelText: 'User Name'),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Enter password', labelText: 'Password'),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 20,),
+        ElevatedButton(
+          child: Text('Login'),
+          style: TextButton.styleFrom(),
+          onPressed: ()=>print('Hai Aswanth'),
+        ),
+
+      ],
+    ));
   }
 }
